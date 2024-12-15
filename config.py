8e6c1c90f7a8bcfc2dd48fc3a6877d516e07dc7c
godlikerazith <rrazithc@gmail.com>
2024-12-16 01:37:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '1918079773').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002423270359'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/All_In_One_Linkz") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
 
 # Repo By @SahedSarker
